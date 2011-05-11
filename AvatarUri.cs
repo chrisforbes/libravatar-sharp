1050f84a1eb6fd9c38485bd574a1789118162fd1
Chris Forbes <chrisf@ijw.co.nz>
2011-05-11 18:32:43 +1200

    add HTTPS and SHA256 options

--- AvatarUri.cs
+++ AvatarUri.cs
@@ -19,7 +19,7 @@ namespace libravatarsharp
 	public static class AvatarUri
 	{	
 		/// <summary>
-		/// Converts an email address to a libravatar URI
+		/// Converts an email address to a libravatar URI, using default options
 		/// </summary>
 		/// <param name="email">
 		/// The email address associated with the avatar you want to retrieve
@@ -29,10 +29,31 @@ namespace libravatarsharp
 		/// </returns>
 		public static Uri FromEmail( string email )
 		{
-			return new Uri(BaseUrl + MD5Hash(email.ToLowerInvariant()));
+			return FromEmail( email, new AvatarOptions {} );
+		}
+
+		/// <summary>
+		/// Converts an email address to a libravatar URI
+		/// </summary>
+		/// <param name="email">
+		/// The email address associated with the avatar you want to retrieve
+		/// </param>
+		/// <param name="options">
+		/// An AvatarOptions object, which provides various ways to customize the behavior
+		/// </param>
+		/// <returns>
+		/// A URI which points to the user's avatar.
+		/// </returns>
+		public static Uri FromEmail( string email, AvatarOptions options )
+		{
+			var baseUrl = options.PreferHttps ? SecureBaseUrl : BaseUrl;
+			var hash = options.UseSHA256 ? (Func<string,string>)SHA256Hash : MD5Hash;
+			
+			return new Uri(baseUrl + hash(email.ToLowerInvariant()));
 		}
 	
 		static readonly string BaseUrl = "http://cdn.libravatar.org/avatar/";
+		static readonly string SecureBaseUrl = "https://cdn.libravatar.org/avatar/";
 		
 		static string MD5Hash( string s )
 		{
@@ -53,6 +74,22 @@ namespace libravatarsharp
 			return new string(h.ComputeHash(bytes)
 			    .SelectMany(a => a.ToString("x2")).ToArray());
 		}
+	}
+	
+	public class AvatarOptions
+	{
+		/// <summary>
+		/// Produce https:// URIs where possible. This avoids mixed-content warnings in browsers
+		/// when using libravatar-sharp from within a page served via HTTPS.
+		/// </summary>
+		public bool PreferHttps;
+		
+		/// <summary>
+		/// Use the SHA256 hash algorithm, rather than MD5. SHA256 is significantly stronger,
+		/// but is not supported by Gravatar, so libravatar's fallback to Gravatar for missing
+		/// images will not work.
+		/// </summary>
+		public bool UseSHA256;
 	}
 }
 
